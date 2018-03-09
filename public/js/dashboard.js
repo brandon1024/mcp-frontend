5e13d306cd17b553cf5372ecc364cfdf28cc321b
Brandon Richardson <brandon1024.br@gmail.com>
2018-03-09 14:12:47 -0500

    done

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,28 @@
 window.onload = () => {
+    /* Update Initially */
+    updateLedger();
+    updateLogs();
+
+    (function reloadRunner() {
+        updateMap();
+        document.getElementById('map-status-text').innerText = 'Reloading in 10s...';
+
+        setTimeout(function() {
+            document.getElementById('map-status-text').innerText = 'Reloading in 5s...';
+        }, 5000);
+
+        setTimeout(function() {
+            document.getElementById('map-status-text').innerText = 'Reloading...';
+        }, 9000);
+
+        setTimeout(reloadRunner, 10000);
+    })();
+
+    document.getElementById('update-ledger-button').addEventListener('click', updateLedger, false);
+    document.getElementById('update-logs-button').addEventListener('click', updateLogs, false);
+};
+
+function updateMap() {
     /*$.ajax({
         type: "get",
         url: "/dashboard/map",
@@ -27,32 +51,27 @@ window.onload = () => {
         }
     });*/
 
-    $.ajax({
-        type: "get",
-        url: "/dashboard/logs",
-        dataType: "json",
-        success: function (logs) {
-            var tableBody = document.getElementById("logs-table-body");
+    //map
+    var COLOR_RED = 'rgba(255, 0, 0, 0.5)';
+    var COLOR_BLUE = 'rgba(0, 0, 255, 0.5)';
+    var renderCircle = function(context, coords, color) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(coords[0], coords[1], 8, 0, Math.PI * 2, true);
+        ctx.fill();
+    };
 
-            for(var index = 0; index < logs.length; index++) {
-                var row = document.createElement("tr");
+    var c = document.getElementById("map-canvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.width);
+    ctx.canvas.width  = c.getBoundingClientRect().width;
+    ctx.canvas.height = 600;
 
-                var col = document.createElement("td");
-                col.innerText = index+1;
-                row.appendChild(col);
+    renderCircle(ctx, [100, 100], COLOR_RED);
+    renderCircle(ctx, [120, 120], COLOR_BLUE);
+}
 
-                var col = document.createElement("td");
-                col.innerText = logs[index].log;
-                row.appendChild(col);
-
-                tableBody.appendChild(row);
-            }
-        },
-        error: function() {
-            console.log('err');
-        }
-    });
-
+function updateLedger() {
     $.ajax({
         type: "get",
         url: "/dashboard/ledger",
@@ -88,4 +107,32 @@ window.onload = () => {
             console.log('err');
         }
     });
-};
+}
+
+function updateLogs() {
+    $.ajax({
+        type: "get",
+        url: "/dashboard/logs",
+        dataType: "json",
+        success: function (logs) {
+            var tableBody = document.getElementById("logs-table-body");
+
+            for(var index = 0; index < logs.length; index++) {
+                var row = document.createElement("tr");
+
+                var col = document.createElement("td");
+                col.innerText = index+1;
+                row.appendChild(col);
+
+                var col = document.createElement("td");
+                col.innerText = logs[index].log;
+                row.appendChild(col);
+
+                tableBody.appendChild(row);
+            }
+        },
+        error: function() {
+            console.log('err');
+        }
+    });
+}
