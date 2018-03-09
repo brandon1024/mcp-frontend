78cbe10c5b03a38f24629c4516a930c9153fef74
Brandon Richardson <brandon1024.br@gmail.com>
2018-03-09 09:58:32 -0500

    Initial commit

---
+++ config/db/knexfile.js
@@ -0,0 +1,63 @@
+/* TODO: Change Database Name to App Name */
+module.exports = {
+    development: {
+        client: 'mysql',
+        connection: {
+            host     : process.env.DB_HOST || '127.0.0.1',
+            user     : process.env.DB_USER || 'root',
+            password : process.env.DB_PASS || 'password',
+            database : 'ceccompetitiondev',
+            charset  : 'utf8'
+        },
+        pool: { min: 2, max: 10 },
+        acquireConnectionTimeout: 10000,
+        debug: false,
+        migrations: {
+            directory: './migrations',
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './seeds/dev'
+        }
+    },
+    test: {
+        client: 'mysql',
+        connection: {
+            host     : process.env.DB_HOST || '127.0.0.1',
+            user     : process.env.DB_USER || 'root',
+            password : process.env.DB_PASS || 'password',
+            database : 'ceccompetitiontest',
+            charset  : 'utf8'
+        },
+        pool: { min: 2, max: 10 },
+        acquireConnectionTimeout: 10000,
+        debug: false,
+        migrations: {
+            directory: './migrations',
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './seeds/test'
+        }
+    },
+    production: {
+        client: 'mysql',
+        connection: {
+            host     : process.env.DB_HOST || '127.0.0.1',
+            user     : process.env.DB_USER || 'root',
+            password : process.env.DB_PASS || 'password',
+            database : 'ceccompetitionprod',
+            charset  : 'utf8'
+        },
+        pool: { min: 2, max: 10 },
+        acquireConnectionTimeout: 10000,
+        debug: false,
+        migrations: {
+            directory: './migrations',
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './seeds/prod'
+        }
+    }
+};
---
+++ routes/dashboard.js
@@ -0,0 +1,28 @@
+/* Retrieve Router Handler */
+const express = require('express');
+const router = express.Router();
+
+/* Debugger */
+const debug = require('debug')('route-home');
+
+module.exports = (app, passport) => {
+    function authenticate(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/login')
+    }
+
+    /* Views */
+    router.get('/', authenticate, function(req, res, next) {
+        res.render('dashboard', {
+            authenticated: req.isAuthenticated()
+        });
+    });
+
+
+    /* API Endpoints */
+
+    /* Register Router */
+    app.use('/dashboard', router);
+};
